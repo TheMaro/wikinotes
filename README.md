48467de80c4bbfebc6550f9f471fa48ca9742309
themaro <markoackovski@gmail.com>
2018-02-18 19:02:43 +0100

    Highlighting test

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# WikiNotes
+# Work in progress
 A small portfolio project
-
-## Wikipedia viewer
-Inspired by fCC
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const xhr = new XMLHttpRequest()
 // WHOLE PAGE
-//let url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Steve+Jobs&prop=text&origin=*'
+let url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Mike+Tyson&prop=text&disableeditsection=true&origin=*'
 // SECTIONS TITLES
 //let url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Steve+Jobs&prop=sections'
 // SPECIFIC SECTION
-let url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Mike+Tyson&section=0&disableeditsection=true&mobileformat=true&origin=*'
+//let url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Steve+Jobs&section=1&disableeditsection=true&mobileformat=true&origin=*'
 
 xhr.open('GET', url, true)
 console.log('OPENED', xhr.status);
@@ -28,18 +28,8 @@ xhr.onload = function () {
     cleanUp(output)
     // Call section printing function
     //printSections(data)
-    
-    
-    // REMOVE LINKS FROM WIKI PAGE
-    Array.from(output.getElementsByTagName('a')).forEach(item => {
-      item.outerHTML = item.innerHTML
-    })
-    Array.from(output.getElementsByTagName('span')).forEach(item => {
-      item.outerHTML = item.innerHTML
-    })
-    Array.from(output.getElementsByTagName('b')).forEach(item => {
-      item.outerHTML = item.innerHTML
-    })
+    output.addEventListener('mouseup', getHighlight)
+
     
   }
 };
@@ -61,17 +51,38 @@ function addNote (e) {
   document.getElementById('notes').innerHTML += note
 
   // Add close btn event
-  document.getElementById('notes').addEventListener('click',deleteNote)
+  
+  
   
   
 }
-
+document.getElementById('notes').addEventListener('click',deleteNote)
+// Note delete button event function
 function deleteNote (e) {
   if (e.target.id === 'delete-note') {
     console.log('delete event')
     e.target.parentElement.parentElement.remove()
   }
 }
+
+// Text highlighter first click
+function getHighlight (e) {
+  text = window.getSelection();
+  if (text != '')  {
+      document.getElementById('notes').innerHTML += `
+      <div class="card text-white bg-dark mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Article: ${test.parse.title}</h5>
+            <p class="card-text" id="notes">${text}</p>
+            <button id="delete-note">delete</button>
+          </div>
+        </div>
+      `
+    }
+  text.empty()
+}
+
+
 // FUNCTION FOR OUTPUTING WIKI PAGE SECTIONS FROM JSON OBJECT
 // function printSections (sections) {
 //   sections.parse.sections.forEach(element => {
@@ -81,11 +92,6 @@ function deleteNote (e) {
 
 // Clean Wiki from all styles and extra HTML
 function cleanUp (html) {
-  Array.from(html.getElementsByClassName('reference')).forEach(item => item.remove())
-  Array.from(html.getElementsByClassName('mw-references-wrap')).forEach(item => item.remove())
-  Array.from(html.getElementsByClassName('hatnote')).forEach(item => item.remove())
-  Array.from(html.getElementsByTagName('a')).forEach(item => item.outerHTML = item.innerHTML)
-  Array.from(html.getElementsByTagName('span')).forEach(item => item.outerHTML = item.innerHTML)
-  Array.from(html.getElementsByTagName('b')).forEach(item => item.outerHTML = item.innerHTML)
-  
+  Array.from(html.querySelectorAll('.reference,.mw-references-wrap,.hatnote')).forEach(item => item.remove())
+  Array.from(html.querySelectorAll('span,a,b')).forEach(item => item.outerHTML = item.innerHTML)
 }
